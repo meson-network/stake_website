2917938b81514c782c5e5b41fc4e40df4b0eeca9
Bruce Wayne <bruce-wayne-business@proton.me>
2024-04-27 13:25:37 +0700

    commit

--- src/config/router/stake.js
+++ src/config/router/stake.js
@@ -1,7 +1,12 @@
 export default [
     {
-        path: "/stake",
-        name: "stake",
-        component: () => import("../../pages/stake/stake.vue"),
+        path: "/stake_mainnet",
+        name: "stake_mainnet",
+        component: () => import("../../pages/stake/stake_eth_mainnet.vue"),
+    },
+    {
+        path: "/stake_sepolia",
+        name: "stake_sepolia",
+        component: () => import("../../pages/stake/stake_sepolia.vue"),
     },
 ];
